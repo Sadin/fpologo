05f42c7e31826cd42ca240e07f36051fe7eadbff
Zach Snyder <sadin@fedoraproject.org>
2022-04-11 14:10:26 -0400

    script modifies svg text, but with hardcoded index

--- src/fpologo/fpologo.py
+++ src/fpologo/fpologo.py
@@ -4,9 +4,15 @@ import xml.etree.ElementTree as et
 
 
 def main() -> int:
-    svg = et.parse('fedora-horiz_whitebg_test.svg')
+    svg = et.parse('./fedora-horiz_whitebg_test.svg')
     root = svg.getroot()
-    print(root)
+    for child in root[2][1][1]:
+        print(child.get('id'))
+        if child.get('id') == 'tspan_main':
+            child.text = 'text_main'
+            child.set('modified', 'yes')
+
+    svg.write('./fedora-horiz_whitebg_test_modified.svg')
     return 0
 
 
