2034c11ce0bb919126f11ef02b6d28af617a455c
Zach Snyder <sadin@fedoraproject.org>
2022-06-09 09:40:52 -0400

    attempt to standardize XML reading
    xml.etree.ElementTree methods are not working properly, attempting to brute force the SVG xml format

--- src/fpologo/fpologo.py
+++ src/fpologo/fpologo.py
@@ -1,18 +1,32 @@
 from __future__ import annotations
 
+import logging
 import xml.etree.ElementTree as et
 
 
 def main() -> int:
-    svg = et.parse('./fedora-horiz_whitebg_test.svg')
-    root = svg.getroot()
+    # configure logging module
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+    # load config
+
+    # svg path
+    path = './fedora-horiz_whitebg_test.svg'
+    tree = et.parse(path)
+    logging.info(f'loading svg from {path}')
+
+    # parse SVG
+    root = tree.getroot()
+    print(root.attrib)
+
+    logging.info('print child information')
     for child in root[2][1][1]:
-        print(child.get('id'))
+        print(child.attrib)
         if child.get('id') == 'tspan_main':
             child.text = 'text_main'
             child.set('modified', 'yes')
 
-    svg.write('./fedora-horiz_whitebg_test_modified.svg')
+    # svg.write('./fedora-horiz_whitebg_test_modified.svg')
     return 0
 
 
